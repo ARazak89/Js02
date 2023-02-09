3394051e4b31738b823451200ec731cfee7aa9d4
ARazak89 <124513107+ARazak89@users.noreply.github.com>
2023-02-09 22:44:08 +0100

    Update myName.js

--- myName.js
+++ myName.js
@@ -1,6 +1,3 @@
-// Déclaration de la 1ere variable Daouda
 let myName = "Daouda";
-// Construction de myStr et concaténation
 let myStr = "My name is " + myName + " and I am well!";
-// Affichage de myStr
 console.log(myStr);
