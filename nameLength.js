89aabfe47690db88a22167a6441580b6215c0d6b
ARazak89 <124513107+ARazak89@users.noreply.github.com>
2023-02-09 22:41:58 +0100

    Update nameLength.js

--- nameLength.js
+++ nameLength.js
@@ -1,7 +1,5 @@
 
 let nameLength = 0;
 const lastName = 'Hadiza';
-// Calcul de la longueur de la chaine de caratère
 nameLength = lastName.length;
-// Affichage de la longueur
 console.log(nameLength);
