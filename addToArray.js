6a40c1845ce953306cb6d6971b11085f99801bba
ARazak89 <124513107+ARazak89@users.noreply.github.com>
2023-02-09 22:34:08 +0100

    Update addToArray.js

--- addToArray.js
+++ addToArray.js
@@ -1,6 +1,3 @@
-// Déclaration du tableau
 const myArray = [["John", 23], ["cat", 2]];
-// Ajout de la nouvelle valeur dans le tableau
 myArray.push(["Cs", 1]);
-// Affichage du tableau
 console.log(myArray);
