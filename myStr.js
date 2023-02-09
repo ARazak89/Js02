4ad49954921f1a49f6250c937fe3af9c2e53c579
ARazak89 <124513107+ARazak89@users.noreply.github.com>
2023-02-09 22:49:46 +0100

    Update myStr.js

--- myStr.js
+++ myStr.js
@@ -1,8 +1,2 @@
-// Déclaration de la 1ere chaine de caractères
-let chaine1 = "This is the start";
-// Déclaration de la 2e chaine de caractères
-let chaine2 = "This is the end";
-// Concaténation des deux chaines de caractères 
-let myStr = chaine1 + " " + chaine2;
-// Affichage du résultat
+let myStr = 'This is the start' + " " + 'This is the end';
 console.log (myStr);
